fecf87bde7249799902123dc5ae5e371d2842701
Gabriel Rodrigues <gabriel_rodrigues_@hotmail.com>
2025-12-01 12:00:56 -0300

    test: SpaceParticles desable

--- src/components/Heros/HeroHome/index.tsx
+++ src/components/Heros/HeroHome/index.tsx
@@ -6,7 +6,7 @@ import SpaceParticles from "@/components/Particles/SpaceParticles";
 export default function HeroHome() {
   return (
     <section className="relative flex min-h-[100dvh] flex-col items-center justify-between overflow-hidden px-3 pt-20 pb-5 text-center md:pt-25 xl:pt-10">
-      <SpaceParticles />
+      {/* <SpaceParticles /> */}
       <div className="pointer-events-none absolute inset-0 bg-gradient-to-b from-purple-900/20 via-blue-900/10 to-transparent" />
       <div className="relative z-10 flex flex-col items-center gap-3 md:gap-4">
         <h1 className="xs:text-3xl max-w-[1400px] bg-gradient-to-r from-cyan-400 via-purple-400 to-pink-500 bg-clip-text text-2xl leading-tight font-extrabold text-transparent drop-shadow-[0_0_30px_rgba(168,85,247,0.4)] sm:text-4xl md:text-5xl lg:text-6xl xl:text-7xl">
